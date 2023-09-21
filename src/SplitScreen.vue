e31c35dea15b60738038312071c0f596f7a0ac71
Nick <nickbray2001@gmail.com>
2023-09-20 22:52:37 -0500

    Split-Screen working, Map integrated but not rendering.

---
+++ src/SplitScreen.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import tableData from '@/Data.vue'
+import PropertyMap from '@/PropertyMap.vue'
+</script>
+
+<template>
+  <div class="split-screen">
+    <tableData class="property-list-container" />
+    <PropertyMap class="property-map-container" />
+  </div>
+</template>
+
+<style>
+.split-screen {
+  display: flex;
+}
+
+.property-list-container {
+  flex: 1;
+  padding: 20px;
+}
+
+.property-map-container {
+  flex: 2;
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,25 +1,32 @@
 <template>
     <div>
         <navbar/>
-        <div class="table">
-            <el-table :data="tableData" style="width: 70%" class="table">
-            <el-table-column prop="date" label="Date" width="150" />
-            <el-table-column label="Delivery Info">
-                <el-table-column prop="name" label="Name" width="120" />
-                <el-table-column label="Address Info">
-                <el-table-column prop="state" label="State" width="120" />
-                <el-table-column prop="city" label="City" width="120" />
-                <el-table-column prop="address" label="Address" />
-                <el-table-column prop="zip" label="Zip" width="120" />
+        <div class="splitscreen">
+            <div class="map">
+              <Map/>
+            </div>
+            <div class="table">
+                <el-table :data="tableData" style="width: 70%" class="table">
+                <el-table-column prop="date" label="Date" width="150" />
+                <el-table-column label="Delivery Info">
+                    <el-table-column prop="name" label="Name" width="120" />
+                    <el-table-column label="Address Info">
+                    <el-table-column prop="state" label="State" width="120" />
+                    <el-table-column prop="city" label="City" width="120" />
+                    <el-table-column prop="address" label="Address" />
+                    <el-table-column prop="zip" label="Zip" width="120" />
+                    </el-table-column>
                 </el-table-column>
-            </el-table-column>
-            </el-table>
+                </el-table>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import navbar from '../../components/Nav.vue'
+import Map from '../../components/Map.vue'
+
 interface Address {
   date: string
   name: string
@@ -90,8 +97,14 @@ const tableData: Address[] = [
 </script>
 
 <style>
-.table {
+.splitscreen {
   display: flex;
+}
+.map {
+  flex: 1;
+}
+.table {
+  flex: 2;
   justify-content: center;
   padding: 20px;
 }
