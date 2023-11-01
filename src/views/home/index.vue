3cad26d487469833825da1a38f889b657630d2a6
Aksheeth I <aksheeth.i@utexas.edu>
2023-11-01 13:50:28 -0500

    survey has been made a popup, and bare bones individual listing page added

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,49 +1,85 @@
 <template>
-  <div>
-    <navbar />
-    <div class="map">
-      <Map :data="tableData"></Map>
-    </div>
-    <div class="table">
-      <el-table :data="tableData" style="width: 100%" class="table">
-        <el-table-column prop="name" label="Name" width="150" />
-        <el-table-column prop="rating" label="Rating" width="150" />
-        <el-table-column prop="rent" label="Rent" width="150">
-          <template v-slot="{ row }">
-            {{ row.rent['2022-2023'][1][1] }}
-            <!-- Displaying the first element for "2022-2023" category -->
-          </template>
-        </el-table-column>
-        <el-table-column prop="address" label="Address" width="150" />
-      </el-table>
+  <div class="listing-container">
+    <!-- Property Name -->
+    <h1 class="property-name">{{ propertyName }}</h1>
+
+    <!-- Rating and Utilities -->
+    <el-row :gutter="20" class="info-row">
+      <el-col :span="12">
+        <h2>Rating</h2>
+        <!-- Displaying rating using Element UI star rating component -->
+        <el-rate v-model="rating" disabled></el-rate>
+        <span>{{ rating }}</span>
+
+      </el-col>
+      <el-col :span="12">
+        <h2>Utilities</h2>
+        <el-tag v-for="utility in utilities" :key="utility">{{ utility }}</el-tag>
+      </el-col>
+    </el-row>
+
+    <!-- Location -->
+    <el-row :gutter="20" class="info-row">
+      <el-col :span="24">
+        <h2>Location</h2>
+        <p>{{ address }}</p>
+      </el-col>
+    </el-row>
+
+    <!-- Scatterplot -->
+    <div class="scatterplot-section">
+      <!-- Placeholder for now -->
+      <div class="scatterplot">Scatterplot goes here</div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts">
-import { reqProperty } from '@/api/property'
-import navbar from '../../components/Nav.vue'
-import Map from '../../components/Map.vue'
-import { onMounted, ref } from 'vue'
-
-const tableData = ref([])
-onMounted(async () => {
-  const res = await reqProperty()
-  tableData.value = res
-})
+<script>
+export default {
+  data() {
+    return {
+      propertyName: "West 26",
+      address: "1234 Vue St, Element City, 00000",
+      utilities: ["gym", "pool", "parking"],
+      rating: 4.5,  // Rating out of 5
+      // ... any other required data for the scatterplot
+    };
+  },
+  // methods, computed, etc. if needed
+};
 </script>
 
-<style>
-.map {
-  flex: 1;
-  width: 50%;
-  height: 100vh;
-  float: left;
+<style scoped>
+.listing-container {
+  padding: 20px;
+  text-align: center; /* Center content */
 }
-.table {
-  flex: 2;
-  width: 50%;
-  height: 100vh;
-  float: left;
+
+.property-name {
+  font-size: 36px;
+  font-weight: bold;
+  margin-bottom: 20px;
+}
+
+.info-row {
+  margin-bottom: 30px; /* Add spacing between rows */
+}
+
+h2 {
+  font-size: 24px;
+  font-weight: normal; /* To differentiate from the main title */
+  margin-bottom: 10px;
+  color: #333; /* Darker color for better contrast */
+}
+
+.scatterplot-section {
+  margin-top: 40px;
+}
+
+.scatterplot {
+  width: 100%;
+  height: 400px;
+  border: 1px solid #ccc;
+  background-color: #f5f5f5;
 }
 </style>
